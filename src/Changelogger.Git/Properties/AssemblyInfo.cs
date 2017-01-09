486b4982b9865e1d2878dd80ec271423a7eb5d53
Norbert Eder <me@norberteder.com>
2017-01-09 22:20:05 +0100

    Extract single version file and set version to 0.0.1

--- src/Changelogger.Git/Properties/AssemblyInfo.cs
+++ src/Changelogger.Git/Properties/AssemblyInfo.cs
@@ -10,7 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Changelogger.Git")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d5e6dd7f-7e80-4e47-9655-2a56722cc6d6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/Version.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyCopyright("Copyright © Norbert Eder 2017")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
