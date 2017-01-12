d3ccb9d294bd7e17048c44b6e55ee4702d9cc6e8
Norbert Eder <me@norberteder.com>
2017-01-12 22:19:05 +0100

    Refactor processing

--- src/Changelogger/Program.cs
+++ src/Changelogger/Program.cs
@@ -15,7 +15,12 @@ namespace Changelogger
             var sw = new Stopwatch();
             sw.Start();
 
-            Parser.Default.ParseArguments<Options>(args).WithParsed(HandleOptions);
+            Parser.Default.ParseArguments<Options>(args).WithParsed((options) =>
+            {
+                var program = new Program();
+                program.HandleOptions(options);
+                program.Start(options);
+            });
 
             sw.Stop();
 
@@ -25,19 +30,22 @@ namespace Changelogger
             Console.ReadKey();
         }
 
-        private static void HandleOptions(Options options)
+        private void HandleOptions(Options options)
         {
             if (options.Verbose)
             {
                 Trace.Listeners.Add(new ConsoleTraceListener());
             }
+        }
 
+        private void Start(Options options)
+        {
             GitInformation info = new GitInformation();
             var repository = info.GetRepositoryInformation(options.RepositoryPath);
 
             var preparer = MessagePrepareFactory.GetMessagePreparer(repository.SortStrategy);
             var logs = preparer.PrepareMessages(repository);
-            
+
             var exporter = LogExporterFactory.GetLogExporter("markdown");
             var val = exporter.GetExportValue(logs);
 
