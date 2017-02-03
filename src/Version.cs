5989b5c9edc05bab01bcba24781033929a9e1efe
Norbert Eder <opensource@norberteder.com>
2017-02-03 13:59:02 +0100

    Fix assembly version reference

--- src/Version.cs
+++ src/Version.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 [assembly: AssemblyCopyright("Copyright © Norbert Eder 2017")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
